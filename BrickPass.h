01723c0c87d5cacee5fe2ac5285e3e7f546d0ee2
Dzhedu <newnolimit256@gmail.com>
2025-09-22 23:44:20 +0300

    Initial h file

---
+++ BrickPass.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+
+class BrickPass {
+public:
+    BrickPass();
+
+    const std::string& GetPunctuation() const;
+    const std::string& GetLatAlfUp() const;
+    const std::string& GetLatAlfLow() const;
+    const std::string& GetDigits() const;
+
+    struct Result {
+        bool success;
+        std::string message;
+
+        static Result Ok();
+        static Result Error(const std::string& msg);
+    };
+
+    void PrintChar() const;
+    Result IsPhraseValid(const std::string& phrase) const;
+    int Encrypt(int salt, int key, std::string phrase) const;
+
+private:
+    std::string AllSymbols_;
+    std::string Punctuation_;
+    std::string LatAlfUp_;
+    std::string LatAlfLow_;
+    std::string Digits_;
+};
