1b2e85f2f4a6cdbd8cd9e7efe28fa619ed7ecc59
Dzhedu <newnolimit256@gmail.com>
2025-09-27 21:33:18 +0300

    Version Format Changed

--- Version.h
+++ Version.h
@@ -2,7 +2,7 @@
 
 constexpr int BRICKPASS_VERSION_MAJOR = 1;
 constexpr int BRICKPASS_VERSION_MINOR = 0;
-constexpr int BRICKPASS_VERSION_PATCH = 3;
+constexpr int BRICKPASS_VERSION_PATCH = 0;
 
-constexpr const char* BRICKPASS_VERSION_STRING = "20250927.1.0.0";
+constexpr const char* BRICKPASS_VERSION_STRING = "1.0.0";
 
